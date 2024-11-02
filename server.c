9cbcd3413062f9525810bfec890d072f0d6a05ef
LouisOporto <louisoporto042@csu.fullerton.edu>
2024-11-01 21:39:11 -0700

    Add socket header

---
+++ server.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/socket.h>
+
+int main(int argc, char* argv[]) {
+  printf("Starting server...\n");
+
+  return 0;
+}
