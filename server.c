241df02f4cde99ca4ce632ef568ab78158100067
LouisOporto <louisoporto042@csu.fullerton.edu>
2024-11-04 08:31:12 -0800

    Argument validation

--- server.c
+++ server.c
@@ -4,8 +4,17 @@
 #include <pthread.h>
 #include <sys/socket.h>
 
+// Usage: ./server <port>
 int main(int argc, char* argv[]) {
-  printf("Starting server...\n");
 
+  if (argc < 2) {
+    printf("Usage: %s <port>\n", argv[0]);
+    return 1;
+  }
+
+  printf("Starting server...\n");
+  
+
+  
   return 0;
 }
